3ae6797a3c4faca51fee1c8185d4b77ca4b9ed7c
valentin stupa <valentin.stupa@ediweb.com>
2023-08-15 16:59:45 +0300

    bug fix

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,7 +18,7 @@ class Triangle{
         if (a +b < c 
         || a + c < b 
         || b + c < a){
-        throw new Error("Треугольник с такими сторонами не сушествет");
+        throw new Error("Треугольник с такими сторонами не существует");
         }
         this.a = a;
         this.b = b;
